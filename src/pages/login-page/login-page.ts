bb91ba99eeb8c85067d2b558f574cf5ca17c556a
aramecki <altair842.yt@gmail.com>
2025-08-12 19:43:55 +0200

    Inital feat/LD commit

--- src/pages/login-page/login-page.ts
+++ src/pages/login-page/login-page.ts
@@ -12,25 +12,25 @@ import {AccessToken} from '@interfaces';
   styleUrl: './login-page.css'
 })
 export class LoginPage {
-  constructor(private readonly authService: Auth) {}
-
-  username: string | null = null;
-  password: string | null = null;
-
-  login(){
-    if(!this.username || ! this.password){
-      //TODO Handle toast message
-      return;
-    }
-
-
-    this.authService.login(this.username, this.password)
-      .subscribe({
-        next: (res: AccessToken) => {
-          sessionStorage.setItem('token', res.token);
-          //TODO handle redirect to internal app
-        }
-      })
-  }
+  // constructor(private readonly authService: Auth) {}
+  //
+  // username: string | null = null;
+  // password: string | null = null;
+  //
+  // login(){
+  //   if(!this.username || ! this.password){
+  //     //TODO Handle toast message
+  //     return;
+  //   }
+  //
+  //
+  //   this.authService.login(this.username, this.password)
+  //     .subscribe({
+  //       next: (res: AccessToken) => {
+  //         sessionStorage.setItem('token', res.token);
+  //         //TODO handle redirect to internal app
+  //       }
+  //     })
+  // }
 
 }
