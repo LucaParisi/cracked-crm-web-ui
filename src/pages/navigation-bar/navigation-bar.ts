25049c860152f3d035d54cc5bea1cd65313fd3fe
aramecki <altair842.yt@gmail.com>
2025-08-22 12:59:58 +0200

    Improved styles.css structure. Finished navbar base style for big screens.

--- src/pages/navigation-bar/navigation-bar.ts
+++ src/pages/navigation-bar/navigation-bar.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {Router} from "@angular/router";
+import {Auth} from '@apis';
 
 @Component({
   selector: 'app-navigation-bar',
     imports: [
         NgOptimizedImage
     ],
-  templateUrl: './navigation-bar.html',
-  styleUrl: './navigation-bar.css'
+  templateUrl: './navigation-bar.html'
 })
 export class NavigationBar {
+
+  constructor( private readonly router: Router) {}
+
+    // This list of routes is temporary just for design purpose
+    pages = [
+      { id: 1, name: "Home", route: "" },
+      { id: 2, name: "Subscriptions", route: "subscriptions" },
+      { id: 3, name: "About", route: "about" },
+      { id: 4, name: "Contacts", route: "contacts" },
+    ]
+
+  navigateToPage(page: string): void {
+    this.router.navigate([page]);
+  }
 
 }
